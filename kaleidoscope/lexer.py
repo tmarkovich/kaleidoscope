451bf7a25cbbd63870bc680c14ba26fd77da781d
Thomas Markovich <thomasmarkovich+git@gmail.com>
2017-07-15 10:06:03 -0400

    Added basic token types

---
+++ kaleidoscope/lexer.py
@@ -0,0 +1,19 @@
+class EOFToken(object):
+    pass
+
+class DefToken(object):
+    pass
+
+class ExternToken(object):
+    pass
+
+class IdentifierToken(object):
+    pass
+
+class NumberToken(object):
+    pass
+
+class CharacterToken(object):
+    """The catchall token for our simple lexer
+    """
+    pass
