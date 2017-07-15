b266d5069cde36e0461acb8a6ff401729c98ade0
Thomas Markovich <thomasmarkovich+git@gmail.com>
2017-07-15 10:14:32 -0400

    Added all tokens
    fix: && -> And

--- kaleidoscope/lexer.py
+++ kaleidoscope/lexer.py
@@ -8,12 +8,36 @@ class ExternToken(object):
     pass
 
 class IdentifierToken(object):
-    pass
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
 
 class NumberToken(object):
-    pass
+    """Number token.
+    """
+    def __init__(self, value):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
 
 class CharacterToken(object):
-    """The catchall token for our simple lexer
+    """The catchall token for our simple lexer.
     """
-    pass
+    def __init__(self, char):
+        self._char = char
+
+    @property
+    def char(self):
+        return self._char
+
+    def __eq__(self, other):
+        return (isinstance(other, CharacterToken) and
+                (self.char == other.char))
+
+    def __ne__(self, other):
+        return not (self == other)
